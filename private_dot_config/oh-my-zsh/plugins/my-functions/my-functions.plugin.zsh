d00699f3bf01e1f7f10bc584707d25c40b899166
CamilleHBP <11173924+CamilleHbp@users.noreply.github.com>
2026-02-04 11:47:32 +0100

    feat: remove deprecated functions and add nnn terminal file manager integration

--- private_dot_config/oh-my-zsh/plugins/my-functions/my-functions.plugin.zsh
+++ private_dot_config/oh-my-zsh/plugins/my-functions/my-functions.plugin.zsh
@@ -8,7 +8,9 @@
 
 os_brew=$(which brew)
 
-brew() {
+# Wrapper around brew to perform additional maintenance tasks after certain actions
+# Specifically, after installing, removing, uninstalling, upgrading, or reinstalling packages, it runs `brew autoremove` and `brew cleanup`, regenerates the Brewfile, and adds it to chezmoi if available.
+function brew() {
     local brew_action=$1
     if [[ $brew_action == "install" ]] || [[ $brew_action == "remove" ]] || [[ $brew_action == "uninstall" ]] || [[ $brew_action == "upgrade" ]] || [[ $brew_action == "reinstall" ]]; then
         echo "----- start: brew $brew_action -----"
@@ -45,15 +47,43 @@ brew() {
     fi
 }
 
+# Git: List the largest files in the git repository along with their paths
 function git_list_files() {
     while read -r largefile; do
         echo $largefile | awk '{printf "%s %s ", $1, $3 ; system("git rev-list --all --objects | grep " $1 " | cut -d \" \" -f 2-")}'
     done <<<"$(git rev-list --all --objects | awk '{print $1}' | git cat-file --batch-check | sort -k3nr | head -n 20)"
 }
 
-function pip_uninstall() {
-    pip install -q pipdeptree
-    pipdeptree -p$1 -fj | jq ".[] | .package.key" | xargs pip uninstall -y
+# [nnn: terminal file manager](https://github.com/jarun/nnn)
+# This function wraps the nnn terminal file manager to enable changing the current shell directory upon exiting nnn.
+function n () {
+    # Block nesting of nnn in subshells
+    [ "${NNNLVL:-0}" -eq 0 ] || {
+        echo "nnn is already running"
+        return
+    }
+
+    # The behaviour is set to cd on quit (nnn checks if NNN_TMPFILE is set)
+    # If NNN_TMPFILE is set to a custom path, it must be exported for nnn to
+    # see. To cd on quit only on ^G, remove the "export" and make sure not to
+    # use a custom path, i.e. set NNN_TMPFILE *exactly* as follows:
+    #      NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+    export NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
+
+    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
+    # stty start undef
+    # stty stop undef
+    # stty lwrap undef
+    # stty lnext undef
+
+    # The command builtin allows one to alias nnn to n, if desired, without
+    # making an infinitely recursive alias
+    command nnn "$@"
+
+    [ ! -f "$NNN_TMPFILE" ] || {
+        . "$NNN_TMPFILE"
+        rm -f -- "$NNN_TMPFILE" > /dev/null
+    }
 }
 
 # Functions will be loaded on-demand when they are first called, rather than at shell startup
